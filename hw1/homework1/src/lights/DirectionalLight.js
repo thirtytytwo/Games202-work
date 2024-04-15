453f6b54557d5eedb8caae95cf4a28b3d208055e
luyicheng <thirtytwo0301@gmail.com>
2024-04-15 19:05:16 +0800

    作业1光源矩阵部分

--- hw1/homework1/src/lights/DirectionalLight.js
+++ hw1/homework1/src/lights/DirectionalLight.js
@@ -23,9 +23,18 @@ class DirectionalLight {
 
         // Model transform
 
+        modelMatrix[0] = scale.x;
+        modelMatrix[5] = scale.y;
+        modelMatrix[10] = scale.z;
+
+        modelMatrix[3] = translate.x;
+        modelMatrix[7] = translate.y;
+        modelMatrix[11] = translate.z;
         // View transform
-    
+        mat4.lookAt(viewMatrix, this.lightPos, this.focalPoint, this.lightUp);
+
         // Projection transform
+        mat4.ortho(projectionMatrix, 100, 100, 100, 100, 0.03, 1000);
 
         mat4.multiply(lightMVP, projectionMatrix, viewMatrix);
         mat4.multiply(lightMVP, lightMVP, modelMatrix);
