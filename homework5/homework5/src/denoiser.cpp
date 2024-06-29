9f0098c6d5472cc37820969da791e57a611d8241
thirtytwo <58972099+thirtytytwo@users.noreply.github.com>
2024-06-29 15:55:47 +0800

    作业5两小节

--- homework5/homework5/src/denoiser.cpp
+++ homework5/homework5/src/denoiser.cpp
@@ -12,9 +12,33 @@ void Denoiser::Reprojection(const FrameInfo &frameInfo) {
 #pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            // TODO: Reproject
+
             m_valid(x, y) = false;
             m_misc(x, y) = Float3(0.f);
+            // TODO: Reproject
+            Float3 position = frameInfo.m_position(x,y);
+            int indexCur = frameInfo.m_id(x, y);
+            if(indexCur == -1){
+                continue;
+            }
+            // 根据这帧的位置，和获取存储上帧每个物体矩阵的对应物体矩阵，上帧摄像机矩阵，上帧投影到屏幕矩阵带入公式获得上帧该物体对应像素的屏幕坐标
+            Matrix4x4 invWorldMatrixCur = Inverse(frameInfo.m_matrix[indexCur]);
+            Matrix4x4 worldMatrixPre = m_preFrameInfo.m_matrix[indexCur];
+            Matrix4x4 screenMatrixpre = preWorldToScreen * preWorldToCamera * worldMatrixPre * invWorldMatrixCur;
+            Float3 screenPositionPre = screenMatrixpre(position, Float3::EType::Point);
+
+            //判断上帧这个物体的这个部分是否是屏幕外
+            if(screenPositionPre.x < 0 || screenPositionPre.x >= width || screenPositionPre.y < 0 || screenPositionPre.y >= height){
+                continue;
+            }
+            else{
+                int indexPre = m_preFrameInfo.m_id(screenPositionPre.x, screenPositionPre.y);
+                // 判断是否上帧和这帧出现了遮挡关系改变，即上帧对应的位置上是别的物体
+                if(indexPre == indexCur){
+                    m_valid(x, y) = true;
+                    m_misc(x, y) = m_accColor(screenPositionPre.x, screenPositionPre.y);
+                }
+            }
         }
     }
     std::swap(m_misc, m_accColor);
@@ -45,8 +69,50 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
 #pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
+            Float3 result;
+            auto totalWeight = .0f;
+
+            int x_start = std::max(0, x - kernelRadius);
+            int x_end = std::min(width - 1, x + kernelRadius);
+            int y_start = std::max(0, y - kernelRadius);
+            int y_end = std::min(height - 1, y + kernelRadius);
+
+            Float3 centerPosition = frameInfo.m_position(x, y);
+            Float3 centerNormal   = frameInfo.m_normal(x, y);
+            Float3 centerColor    = frameInfo.m_beauty(x, y);
+
+            for(int i = x_start; i <= x_end; i++) {
+                for(int j = y_start; j <= y_end; j++) {
+                    Float3 curPosition = frameInfo.m_position(i, j);
+                    Float3 curNormal   = frameInfo.m_normal(i, j);
+                    Float3 curColor    = frameInfo.m_beauty(i, j);
+                    
+                    // Position
+                    auto positionD = SqrDistance(centerPosition, curPosition) / (2.0f * m_sigmaCoord * m_sigmaCoord);
+
+                    // Color
+                    auto colorD = SqrDistance(centerColor, curColor) / (2.0f * m_sigmaColor * m_sigmaColor);
+
+                    // Normal
+                    auto normalD = SafeAcos(Dot(centerNormal, curNormal));
+                    normalD *= normalD;
+                    normalD / (2.0f * m_sigmaNormal * m_sigmaNormal);
+
+                    // Plane
+                    float planeD = .0f;
+                    if(positionD > 0.f){
+                        planeD = Dot(centerNormal, Normalize(curPosition - centerPosition));
+                    }
+                    planeD *= planeD;
+                    planeD /= (2.0f * m_sigmaPlane * m_sigmaPlane);
+
+                    float weight = std::exp(-(positionD + colorD + normalD + planeD));
+                    totalWeight += weight;
+                    result += curColor * weight;
+                }
+            }
             // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            filteredImage(x, y) = result / totalWeight;
         }
     }
     return filteredImage;
